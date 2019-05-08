20bfc409df6a3090609777abeae2e43ebedbc067
yuliang <secondgalaxy@163.com>
2019-05-08 10:28:55 +0800

    添加AspNetCore Api项目

---
+++ CommonQuery.Api.NetCoreExample/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommonQuery.Builder;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommonQuery.Api.NetCoreExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpPost]
+        public ActionResult<IEnumerable<string>> Post(BaseQueryBuilder qb)
+        {
+
+            IQueryable<User> users = new User().GetUsers().Where(qb);
+            return this.Ok(users);
+        }
+
+
+    }
+
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public IQueryable<User> GetUsers()
+        {
+            return new List<User>
+            {
+                new User {Id = 1,Name = "Nigel"},
+                new User {Id=2,Name = "Jim"}
+            }.AsQueryable();
+        }
+    }
+}
---
+++ CommonQuery.Api.NetCoreExample/Startup.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace CommonQuery.Api.NetCoreExample
+{
+    /// <summary>
+    /// Class Startup.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        /// <summary>
+        /// Gets the configuration.
+        /// </summary>
+        /// <value>The configuration.</value>
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// Configures the services.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CommonQuery.Api.NetCoreExample", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+
+                c.DescribeAllEnumsAsStrings();
+
+                c.DescribeAllParametersInCamelCase();
+
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// Configures the specified application.
+        /// </summary>
+        /// <param name="app">The application.</param>
+        /// <param name="env">The env.</param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "CommonQuery.Api.NetCoreExample"); });
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
